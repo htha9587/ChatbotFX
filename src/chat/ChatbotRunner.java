d862502430ce67c9e74a6c58f101b90f09145a91
htha9587 <harrison.thacker69@gmail.com>
2016-07-05 18:24:02 -0600

    Version 0.2
    Added basis for FXML layout and got Main Class to work.

---
+++ src/chat/ChatbotRunner.java
@@ -0,0 +1,65 @@
+package chat;
+
+
+import java.io.IOException;
+
+/**
+ * Version 0.2
+ * @author htha9587
+ * 7-5-16
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class ChatbotRunner extends Application 
+{
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+	/**
+	 * Sets stage with the scene.
+	 */
+	@Override
+	public void start(Stage primaryStage) 
+	{	
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("ChatbotFX");
+		
+		initRootLayout();
+	}
+	
+	public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader(ChatbotRunner.class.getResource("view/ChatbotView.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+	
+ /**
+  * Returns main stage.
+  */
+ public Stage getPrimaryStage()
+ {
+	 return primaryStage;
+ }
+ 
+	/**
+	 * Runs the program.
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		launch(args);
+	}
+}
