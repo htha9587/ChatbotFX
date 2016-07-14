c560e2c97c2d9b63a4e31c87af24b57b03de6d7f
htha9587 <harrison.thacker69@gmail.com>
2016-07-14 14:59:17 -0600

    Version 2.1
    Finished creating Constructor for ChatbotFXTwitter,.

--- src/chat/view/ChatbotMapController.java
+++ src/chat/view/ChatbotMapController.java
@@ -8,7 +8,7 @@ import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
 /**
- * Version 2.0
+ * Version 2.1
  * @author htha9587
  * 7-14-16
  */
--- src/chat/view/ChatbotViewController.java
+++ src/chat/view/ChatbotViewController.java
@@ -26,7 +26,7 @@ import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 /**
- * Version 2.0
+ * Version 2.1
  * @author htha9587
  * 7-14-16
  */
